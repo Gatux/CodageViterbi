394f60cb0287e8eb8d1941ee04e89bdc9e45311c
RainbowLyte <rainbowlyte42@gmail.com>
2014-10-31 22:27:23 +0100

    Ajout de commentaires sur codconv.m + Première version du rapport pour la partie sur l'encodage

--- codconv.m
+++ codconv.m
@@ -4,14 +4,18 @@ function [ c ] = codconv( m, g )
     % codeur
     [G, M] = paramconv(g);
     
-    % La taille 
+    % On calcule d'avance la taille de la matrice c qui va contenir la
+    % séquence de bits à transmettre sur le canal
     len = max([length(G(1,:)) + length(m) - 1, length(G(1,:)), length(m)]);
     c = zeros(length(g), len);
     
+    % La nième ligne de la matrice sera égale au produit de convolution 
+    % modulo 2 du vecteur de bits m et des coefficients du polynômes g_n
     for i=1:length(g)
       c(i,:) = mod(conv(m, G(i,:)), 2);
     end
     
-    c = reshape(c, 1, length(c)*length(c(:,1)))
+    % Séquence de bits finale à transmettre
+    c = reshape(c, 1, length(c)*length(c(:,1)));
 end
 
